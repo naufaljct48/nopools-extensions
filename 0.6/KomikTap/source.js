785b5861c278856e90adad18ecfb47e9abdc6eda
Naufal <naufaljct48@gmail.com>
2023-06-23 00:30:14 +0700

    Update source.js

--- 0.6/KomikTap/source.js
+++ 0.6/KomikTap/source.js
@@ -1160,12 +1160,12 @@ class KomikTapMain extends paperback_extensions_common_1.Source {
          * Selector Default = "h2:contains(Popular Today)"
         */
         this.homescreen_PopularToday_enabled = true;
-        this.homescreen_PopularToday_selector = 'h2:contains(Terpopuler Hari Ini)';
+        this.homescreen_PopularToday_selector = 'h2:contains(Popular Today)';
         this.homescreen_LatestUpdate_enabled = true;
-        this.homescreen_LatestUpdate_selector_box = 'h2:contains(Rilisan Terbaru)';
+        this.homescreen_LatestUpdate_selector_box = 'h2:contains(Latest Update)';
         this.homescreen_LatestUpdate_selector_item = 'div.uta';
         this.homescreen_NewManga_enabled = true;
-        this.homescreen_NewManga_selector = 'h3:contains(Serial baru)';
+        this.homescreen_NewManga_selector = 'h3:contains(Project Update)';
         this.homescreen_TopAllTime_enabled = true;
         this.homescreen_TopAllTime_selector = 'div.serieslist.pop.wpop.wpop-alltime';
         this.homescreen_TopMonthly_enabled = true;
