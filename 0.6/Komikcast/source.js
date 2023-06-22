b82aab4388e0f0b3668c12d0205c6cfdfc99186b
Naufal <naufaljct48@gmail.com>
2023-06-22 15:32:54 +0700

    add shinigami

--- 0.6/Komikcast/source.js
+++ 0.6/Komikcast/source.js
@@ -1017,7 +1017,7 @@ exports.KomikcastMain = exports.getExportVersion = void 0;
 const paperback_extensions_common_1 = require("paperback-extensions-common");
 const KomikcastMainParser_1 = require("./KomikcastMainParser");
 // Set the version for the base, changing this version will change the versions of all sources
-const BASE_VERSION = '1.0.7';
+const BASE_VERSION = '1.0.0';
 const getExportVersion = (EXTENSION_VERSION) => {
     return BASE_VERSION.split('.').map((x, index) => Number(x) + Number(EXTENSION_VERSION.split('.')[index])).join('.');
 };
